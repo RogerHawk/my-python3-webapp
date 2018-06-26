b7dffd92777e397a1ccd79697a6cc459b0fcddc4
rogerhawk <roger996@163.com>
2018-06-26 17:53:00 +0800

    修正期中的语法错误

--- www/handlers.py
+++ www/handlers.py
@@ -1,8 +1,18 @@
-#同样clone自github
+#URL处理
 
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+import re,time,json,logging,hashlib,base64,asyncio
 
-__author__ = 'Michael Liao'
+from coroweb import get,post
 
-' url handlers '
+from model import User,Comment,Blog,next_id
+
+#使用orm中提供的装饰器get。在aiohttp收到对根/的访问的GET访问请求是，controller调用本index，并返回一个dict
+@get('/')
+async def index(request):
+    #异步方式提取User表里所有的数据，以list方式返回users
+    users = await User.findAll()
+    #返回dict类型。交由拦截器response_factory处理
+    return {
+        '__template__':'test.html',
+        'users':users
+    }
